a170124bcc8f4685ffae010d45c544a6aea68dc9
ken <block24block@gmail.com>
2020-05-05 16:08:32 +0800

    auto draw graphs

--- lib/chromark/bookmark_file.rb
+++ lib/chromark/bookmark_file.rb
@@ -45,23 +45,44 @@ class Chromark
       @entries ||= []
     end
 
-    def dup_names
-      return @dup_names if @dup_names
-
-      @dup_names = {}
-      entries.group_by(&:name).each do |name, es|
-        next if es.size == 1
-        @dup_names[name] = es.map(&:path_str)
-      end
-      @dup_names
+    def stats
+      Stat.sub_stats.map{ |sub| sub.new(self) }
     end
 
-    def favorite_hosts
-      @favorite_hosts ||= entries.group_by(&:host).
-        map{ |host, entries| [host, entries.size] }.
-        select{ |h| h[1] > 1 }.
-        sort{ |h1, h2| h2[1] <=> h1[1] }.
-        to_h
+    class Stat
+      attr_reader :file, :data
+
+      class << self
+        attr_reader :sub_stats
+
+        def inherited(base)
+          (@sub_stats ||= []) << base
+        end
+      end
+
+      def initialize(file)
+        @file = file
+        @data = analyze
+      end
+
+      def group_by(elements, attr)
+        elements.group_by(&attr).
+          select{ |name, entries| entries.size > 1 }.
+          sort{ |e1, e2| e2[1].size <=> e1[1].size }.
+          to_h
+      end
+    end
+
+    class DupNames < Stat
+      def analyze
+        group_by(file.entries, :name)
+      end
+    end
+
+    class FavoriteHosts < Stat
+      def  analyze
+        group_by(file.entries, :host)
+      end
     end
 
   end
